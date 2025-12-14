47395cebc5b60ffc3046425ee412086e45263e79
Soy_DANY <jordanymtz.2001@gmail.com>
2025-12-14 12:31:27 -0600

    Agregando la estructura del Fronted

---
+++ Semana 4/FRONTED-Angular/SuperMercado/src/app/Componente/Departamento/detalle-empleado/detalle-empleado.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-empleado',
+  imports: [],
+  templateUrl: './detalle-empleado.html',
+  styleUrl: './detalle-empleado.css',
+})
+export class DetalleEmpleado {
+
+}
---
+++ Semana 4/FRONTED-Angular/SuperMercado/src/app/Componente/Tienda/listar-detalles/listar-detalles.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-detalles',
+  imports: [],
+  templateUrl: './listar-detalles.html',
+  styleUrl: './listar-detalles.css',
+})
+export class ListarDetalles {
+
+}
